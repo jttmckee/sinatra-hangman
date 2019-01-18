ff8744fe057214aaa9455fd2be50e24dd4a91e3e
jttmckee <wp.jason@hotmail.co.uk>
2019-01-18 15:02:16 +0000

    Basic front end

--- hangman.rb
+++ hangman.rb
@@ -1,81 +1,11 @@
 require 'yaml'
 class Hangman
 
-  def play
+  attr_reader :hasWon, :lives, :letters, :incorrect_letters
 
-    hasWon = false
-
-    letter = ""
-    until (@lives<=0 || hasWon)
-      printLetters()
-       loop do
-         letter = getLetter
-
-        if @incorrect_letters.include?(letter) or @letters.include?(letter)
-          puts "You already guessed that letter."
-        elsif letter =~ /[A-Z]/
-          break
-        elsif letter == '+'
-          save_game
-        else
-          puts "That's not a letter!"
-        end
-      end
-
-      puts "You guessed \"#{letter}\""
-      if @word.include? letter
-        puts "That's right! Well done..."
-        @letters << letter
-        if @word.split('').all? {|chr| @letters.include?(chr)} then hasWon = true end
-      else
-        @lives -= 1
-        puts "That's wrong! You have #{@lives} lives left."
-        @incorrect_letters << letter
-
-      end
-
-    end
-
-    if hasWon
-      puts "You win!"
-    else
-
-      puts "You lose!"
-    end
-    puts "The word is \"#{@word}\""
-  end
 
   def initialize
-    setup
-
-    play
-
-  end
-
-  private
-
-  def getLetter
-    puts "Guess a letter (A - Z).  Only the first letter counts.  Type \"+\" to save."
-    letter =  gets[0].upcase
-
-  end
-
-  def printLetters()
-    print "\n"
-    (5 - @lives).times do |i|
-      puts @scaffold[i]
-    end
-    puts "\n"
-
-    print "WORD:"
-    @word.each_char do |chr|
-      print @letters.include?(chr) ? chr : '_'
-      print ' '
-    end
-
-  end
-
-  def setup
+    @hasWon = false
     @lives = 5
     @scaffold = []
     @scaffold << "     --------"
@@ -91,16 +21,48 @@ class Hangman
     until (@word.size <= 11 && @word.size >= 5)
       @word = words[rand(words.size)].chomp.upcase
     end
+  end
+
+  def checkLetter(letter)
+    message = ""
+    if @incorrect_letters.include?(letter) or @letters.include?(letter)
+      message << "You already guessed that letter.\n<BR>"
+    elsif letter =~ /[A-Z]/
+      message << "You guessed \"#{letter}\""
+      if @word.include? letter
+        message << "That's right! Well done..."
+        @letters << letter
+        hasWon = true if @word.split('').all? {|chr| @letters.include?(chr)}
+      else
+        @lives -= 1
+        message << "That's wrong!\n"
+        @incorrect_letters << letter
+      end
+    else
+      message << "That's not a letter"
+    end
+    message << "You have #{@lives} lives left."
+    message << "Already guessed: #{incorrect_letters.join(', ')}"
 
   end
 
+  def printLetters()
+    display = "\n<BR>"
+    (5 - @lives).times do |i|
+      display << "#{@scaffold[i]} \n<BR>"
+    end
+    display << "\n<BR>"
+
+    display << "WORD: "
+    @word.each_char do |chr|
+      display << (@letters.include?(chr) && chr ? chr : '_')
+      display << ' '
+    end
+    display
+  end
+
   def save_game
-    puts "Game saved"
-
-    f = File.open('game.save','w')
-    f.puts YAML::dump(self)
-    f.close
-
+    YAML::dump(self)
   end
 
 end
--- home.rb
+++ home.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require './hangman.rb'
+require 'YAML'
 
 enable :sessions
 
 get '/' do
-  "value = #{session[:value].inspect}"
-end
-
-get '/:value' do
-  session['value'] = params['value']
+  if session[:game] then hangman = YAML::load(session['game'])
+  else hangman = Hangman.new
+  end
+  message = params['letter'] ?
+    hangman.checkLetter(params['letter'][0].upcase ) : "Please type a letter"
+  display = hangman.printLetters
+  session[:game] = hangman.save_game
+  erb :home, locals: {message: message, display: display}
 end
